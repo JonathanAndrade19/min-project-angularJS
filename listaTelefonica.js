0d252ca4c9e8ae48c159eafadc284081079c1dca
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-01-02 18:01:08 -0300

    insert new contacts into the contacts array

--- listaTelefonica.js
+++ listaTelefonica.js
@@ -1,13 +1,15 @@
 angular.module('listaTelefonica', []);
 angular.module('listaTelefonica').controller("listaTelefonicaCtrl", function($scope){
     $scope.titulo = "Lista Telefonica";
-    // console.log($scope.titulo);
 
     $scope.contatos = [
         {nome: "Jonathan Moreira", telefone: "988770099"},
         {nome: "Lídia Araújo", telefone: "933224455"},
         {nome: "Luna Andrade", telefone: "977550011"},
     ];
-    // console.log($scope.contatos);
     
+    $scope.adicionarContato = function (contato) {
+        $scope.contatos.push(angular.copy(contato));
+        delete $scope.contato;
+    };
 });
