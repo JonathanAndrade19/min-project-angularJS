de76d3d27883c4b332ea18a6acc4e96c6067cb85
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-01-01 21:48:11 -0300

    creating table of contact

--- listaTelefonica.js
+++ listaTelefonica.js
@@ -2,4 +2,12 @@ angular.module('listaTelefonica', []);
 angular.module('listaTelefonica').controller("listaTelefonicaCtrl", function($scope){
     $scope.titulo = "Lista Telefonica";
     // console.log($scope.titulo);
+
+    $scope.contatos = [
+        {nome: "Jonathan Moreira", telefone: "988770099"},
+        {nome: "Lídia Araújo", telefone: "933224455"},
+        {nome: "Luna Andrade", telefone: "977550011"},
+    ];
+    // console.log($scope.contatos);
+    
 });
