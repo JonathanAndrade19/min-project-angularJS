5c6695a4f8cb6b972ec8a3850b2a399fae99dcaa
JonathanAndrade19 <jonathanjampamb@gmail.com>
2022-01-03 22:13:05 -0300

    inserting category into the contacts object array

--- listaTelefonica.js
+++ listaTelefonica.js
@@ -9,10 +9,12 @@ angular.module('listaTelefonica').controller("listaTelefonicaCtrl", function($sc
     ];
 
     $scope.operadoras = [
-        {nome: "Oi", codigo: "14"},
-        {nome: "Vivo", codigo: "15"},
-        {nome: "Claro", codigo: "44"},
-        {nome: "Tim", codigo: "41"}
+        {nome: "Oi", codigo: "14", categoria: "Celular"},
+        {nome: "Vivo", codigo: "15", categoria: "Celular"},
+        {nome: "Claro", codigo: "44", categoria: "Celular"},
+        {nome: "Tim", codigo: "41", categoria: "Celular"},
+        {nome: "GVT", codigo: "25", categoria: "Fixo"},
+        {nome: "Embratel", codigo: "21", categoria: "Fixo"}
     ]
     
     $scope.adicionarContato = function (contato) {
